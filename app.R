b44acab69939e66345a730f933d8a91ad2c0d0d9
Som Bohora <som-bohora@ouhsc.edu>
2018-12-29 12:29:30 -0600

    keep progressing

--- app.R
+++ app.R
@@ -2,14 +2,15 @@ mdls <- list('svmLinear'='svmLinear',
              'svmPoly'='svmPoly',
              'Neural Network'='nnet',
              'randomForest'='rf',
-             'k-NN'='knn',
-             'Naive Bayes'='nb',
-             'GLM'='glm',
-             'GAM'='gam')
+             'k-NN'='knn'
+             # 'Naive Bayes'='nb',
+             # 'GLM'='glm',
+             # 'GAM'='gam'
+             )
 
 mdli <- list(
-  'Regression'=c(T,T,T,T,T,F,T,F),
-  'Classification'=c(T,T,T,T,T,T,F,F)
+  'Regression'=c(T,T,T,T,T),
+  'Classification'=c(T,T,T,T,T)
 )  
 
 reg.mdls <- mdls[mdli[['Regression']]]
@@ -46,15 +47,15 @@ Xi <- complete.cases(X)
     
 df2 <- cbind(y,X)[yi&Xi,]
 
-c <- class(df2$y)
-lvls <- length(unique(df2$y))
-if(lvls<10|(c!='numeric'&c!='integer')){
-  modelType <<-'Classification'
-  df2$y <- factor(df2$y)
-} else {
-  modelType <<-'Regression'
-  # if(input$chk_logY){df2$y <- log(df2$y+0.1)}
-}
+# c <- class(df2$y)
+# lvls <- length(unique(df2$y))
+# if(lvls<10|(c!='numeric'&c!='integer')){
+#   modelType <<-'Classification'
+#   df2$y <- factor(df2$y)
+# } else {
+#   modelType <<-'Regression'
+#   # if(input$chk_logY){df2$y <- log(df2$y+0.1)}
+# }
 
 trainIndex <- caret::createDataPartition(df2$y,
                                 p = testsize,
@@ -75,9 +76,9 @@ tuneParams <- list(
   'svmPoly'= expand.grid(degree=1:3,scale=c(0.01,0.1),C=c(0.25,0.5,1)),
   'nnet'=expand.grid(size=c(1,3,5),decay=c(0.01,0.1,1)),
   'rf'=data.frame(mtry=c(2,3,4)),
-  'knn'=data.frame(k=c(1,3,5,7,9)),
-  'nb'=expand.grid(usekernel=c(T,F),adjust=c(0.01,0.1,1),fL=c(0.01,0.1,1)),
-  'glm'=NULL#data.frame()
+  'knn'=data.frame(k=c(1,3,5,7,9))
+  # 'nb'=expand.grid(usekernel=c(T,F),adjust=c(0.01,0.1,1),fL=c(0.01,0.1,1)),
+  # 'glm'=NULL#data.frame()
 )
 
 
@@ -113,29 +114,109 @@ trainArgs <- list(
              preProcess = c('scale','center'),
              method = 'knn',
              trControl = fitControl,
-             tuneGrid=tuneParams[['knn']]),
-  'nb'= list(form=y ~ .,
-            data = dataTrain,
-            preProcess = c('scale','center'),
-            method = 'nb',
-            trControl = fitControl,
-            tuneGrid=tuneParams[['nb']]),
-  'glm'= list(form=y ~ .,
-             data = dataTrain,
-             preProcess = c('scale','center'),
-             method = 'glm',
-             trControl = fitControl,
-             tuneGrid=NULL),
-  'gam'= list(form=y ~ .,
-             data = dataTrain,
-             preProcess = c('scale','center'),
-             method = 'gam',
-             trControl = fitControl)
+             tuneGrid=tuneParams[['knn']])
+  # 'nb'= list(form=y ~ .,
+  #           data = dataTrain,
+  #           preProcess = c('scale','center'),
+  #           method = 'nb',
+  #           trControl = fitControl,
+  #           tuneGrid=tuneParams[['nb']])
+  # 'glm'= list(form=y ~ .,
+  #            data = dataTrain,
+  #            preProcess = c('scale','center'),
+  #            method = 'glm',
+  #            trControl = fitControl,
+  #            tuneGrid=NULL),
+  # 'gam'= list(form=y ~ .,
+  #            data = dataTrain,
+  #            preProcess = c('scale','center'),
+  #            method = 'gam',
+  #            trControl = fitControl)
 )
+
+library(caret)
 
 tune <- lapply(mdls,function(m){
   do.call('train',trainArgs[[m]])
 })
+
+get_fits <- list()
+one_fit <- list()
+
+for (i in names(tune)){
+  one_fit <- tune[[i]]$results
+  # two_fit <- one_fit[(ncol(one_fit)-3):ncol(one_fit)]
+  tidyr::unite_('Model', names(.)[!names(.) %in% fit_names], sep='-', remove=T)
+  get_fits <- rbind(get_fits,one_fit)
+}
+
+
+
+fit_names <- names(one_fit)[(ncol(one_fit)-5):ncol(one_fit)]
+
+df %>% 
+  dplyr::select(model, dplyr::everything()) %>% 
+  tidyr::unite_('Model', names(.)[!names(.) %in% fit_names], sep='-', remove=T)
+
+names(tune)
+
+
+
+
+
+one_fit <- tune[[i]]$results
+# two_fit <- one_fit[(ncol(one_fit)-3):ncol(one_fit)]
+tidyr::unite_('Model', names(.)[!names(.) %in% fit_names], sep='-', remove=T)
+get_fits <- rbind(get_fits,one_fit)
+
+
+
+
+
+
+
+
+tune$svmLinear$results
+df <- tune$svmPoly$results
+df$model <- names(tune)[1]
+
+
+
+
+
+tune$`Neural Network`$results
+df <- tune$`k-NN`$results
+df[1:(ncol(df)-4)]
+
+df[(ncol(df)-3):ncol(df)]
+
+
+library(magrittr)
+
+getRes <- function(i){
+    name <- names(tune)[i]
+    res <- tune[[i]]$results
+    df <- res[(ncol(res)-3):ncol(res)]
+    apply(res,1,function(r) paste(r[1:(ncol(res)-4)],collapse = '-')) %>% 
+      paste(name,.,sep='-') -> model
+    cbind.data.frame(model,df,name=name[[1]],stringsAsFactors =F)
+  }
+    
+    df <- plyr::ldply(1:length(tune),getRes)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 do.call(caret::train(mdls, trainArgs[[1]]))
 
